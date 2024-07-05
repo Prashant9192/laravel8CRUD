210d6195769b747313d7196a4480d0d1e01805e7
Prashant <prashantbasnet222@gmail.com>
2024-07-05 17:18:37 +0530

    worked on seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = new \App\Models\Product([
+            'name' => 'Product 1',
+            'details' => 'Product 1 detail'
+        ]);
+
+        $product->save();
+    }
+}
