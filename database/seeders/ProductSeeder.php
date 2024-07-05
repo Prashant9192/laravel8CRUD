0bde783ff7e918ce2e9dd7a0e8b76affcbfed3f8
Prashant <prashantbasnet222@gmail.com>
2024-07-05 17:21:15 +0530

    add 7 data for test in products

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,5 +19,41 @@ class ProductSeeder extends Seeder
         ]);
 
         $product->save();
+        $product = new \App\Models\Product([
+            'name' => 'Product 2',
+            'details' => 'Product 2 detail'
+        ]);
+
+        $product->save();
+        $product = new \App\Models\Product([
+            'name' => 'Product 3',
+            'details' => 'Product 3 detail'
+        ]);
+
+        $product->save();
+        $product = new \App\Models\Product([
+            'name' => 'Product 4',
+            'details' => 'Product 4 detail'
+        ]);
+
+        $product->save();
+        $product = new \App\Models\Product([
+            'name' => 'Product 5',
+            'details' => 'Product 5 detail'
+        ]);
+
+        $product->save();
+        $product = new \App\Models\Product([
+            'name' => 'Product 6',
+            'details' => 'Product 6 detail'
+        ]);
+
+        $product->save();
+        $product = new \App\Models\Product([
+            'name' => 'Product 7',
+            'details' => 'Product 7 detail'
+        ]);
+
+        $product->save();
     }
 }
